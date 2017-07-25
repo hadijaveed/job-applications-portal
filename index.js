b7b5503aa4df8e891038ea16086f6c7a593f0f35
hadijaveed <hjaveed@knifecenters.com>
2017-07-25 14:33:13 -0400

    remove jquery depenency

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ stickyElement.style.position = 'fixed';
 stickyElement.style.width = stickyElement.parentNode.offsetWidth + 'px';
 
 window.addEventListener('resize', () => {
-    console.log('resizing window here ');
+   
     stickyElement.style.position = 'fixed';
     stickyElement.style.width = stickyElement.parentNode.offsetWidth + 'px';
 });
